340a9496dbba6ad23a38a6b0f2ce058545928c92
VINÍCIUS DE BONA RUAS <vinicius_ruas@estudante.sesisenai.org.br>
2025-12-04 08:19:34 -0300

    acho q deu

--- src/server/app.js
+++ src/server/app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+app.options('*', cors()) // handle preflight requests
 
 // ------------------ Rotas ------------------
 const usuarioRoutes = require('../routes/usuario.routes')

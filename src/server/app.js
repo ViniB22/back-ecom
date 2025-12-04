0712f5f5d3bb89c4e7e803829ecbc72f6709e5fc
VINÍCIUS DE BONA RUAS <vinicius_ruas@estudante.sesisenai.org.br>
2025-12-04 11:17:50 -0300

    afff

--- src/server/app.js
+++ src/server/app.js
@@ -7,7 +7,6 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
-app.options('/*', cors()) // handle preflight requests
 
 // ------------------ Rotas ------------------
 const usuarioRoutes = require('../routes/usuario.routes')
